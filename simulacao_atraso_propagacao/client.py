ca9adbde502245e893734407a7c02886acfa085f
Vitor Cavalcante <v9cavalcante@gmail.com>
2022-12-28 23:24:42 -0300

    simulacao atraso propagacao refeito

--- simulacao_atraso_propagacao/client.py
+++ simulacao_atraso_propagacao/client.py
@@ -4,15 +4,87 @@ import random
 import time
 import sys
 import atexit
+from threading import Timer, Thread
+from aimd import Aimd
 
 client = None
 addr = None
 connectedWithServer = False
 
+BUFFER_SIZE = 3
+buffer = []
+order = 0
+duplicated_acks_count = ("", 0)
+
+timer = None
+resend = False
+rwnd = 1
+aimd = Aimd()
+
+buffer_rtt = {}
+estimated_rtt = 1
+dev_rtt = 1
+timeout_interval = 10
+
 #Função que gerar um número aleatório
 def generateRandomNumber(begin_number, number_of_decimals):
     random_int = random.randrange(begin_number, ((10**number_of_decimals) -1))
     return random_int
+
+#Função que inicia o timer
+def newTimer():
+    global timer
+    timer = Timer(timeout_interval, timeout)
+
+#Função que reseta o timer
+def resetTimer():
+    global timer
+    if timer != None: 
+        timer.cancel()
+    newTimer()
+    timer.start()
+
+#Função executada quando ocorre timeout
+def timeout():
+    global buffer
+    global resend
+    global timeout_interval
+
+    resend = True
+
+    aimd.timeout()
+    resendPacket()
+
+#Função que calcula o intervalo de timeout
+def calculateTimeoutInterval():
+    global estimated_rtt
+    global dev_rtt
+    global timeout_interval
+    global buffer_rtt
+
+    sum_rtt = 0
+    qtd_rtt = 0
+
+    theta = 0.125
+    beta = 0.25
+
+    #Obtém a soma dos RTT
+    for message in buffer_rtt:
+        if "stop" in buffer_rtt[message]:
+            rtt = buffer_rtt[message]["stop"] - buffer_rtt[message]["start"]
+            sum_rtt += rtt
+            qtd_rtt += 1  
+    
+    #Realiza a média dos RTT e faz a estimação
+    if qtd_rtt > 0:
+        sample_rtt = sum_rtt / qtd_rtt
+        estimated_rtt = (1 - theta) * estimated_rtt + theta * sample_rtt
+        dev_rtt = (1 - beta) * dev_rtt + beta * abs(sample_rtt - estimated_rtt)
+        # +3 por conta do mini intervalo entre o recebimento dos pacotes
+        timeout_interval = estimated_rtt + 4 * dev_rtt + 3
+
+    #Reseta o buffer
+    buffer_rtt = {}
 
 #Função para adicionar um cabeçalho IP e enviar o pacote para o roteador
 def sendPacket(address, message):
@@ -25,9 +97,93 @@ def sendPacket(address, message):
     IP_header = source_ip + "|" + destination_ip
     packet = IP_header + "|" + message
 
+    #Salva mensagem no buffer
+    if message.find('connect') == -1:
+      buffer.append(message)
+      buffer_rtt[message] = {
+        "start": time.perf_counter()
+      }
+
     #Envia o pacote para o roteador
     router = ("127.0.0.1", 8100)
     client.sendto(packet.encode("utf-8"), router)
+
+    print(f"Mensagem enviada para o servidor: {message}")
+
+#Função que reenvia os pacotes do buffer caso ocorra timeout
+def resendPacket():
+    global rwnd
+    global queue_resend
+    global timer
+    global resend
+    global buffer
+    global next_resend    
+
+    if rwnd > 0:
+        print('----------------- Pacotes reenviados ----------------')    
+        #Obtém endereço da instância
+        source_ip = client.getsockname()
+        source_ip = source_ip[0] + ":" + str(source_ip[1])
+
+        #Adiciona o cabeçalho IP no pacote
+        destination_ip = addr[0] + ":" + str(addr[1])
+        IP_header = source_ip + "|" + destination_ip
+
+        #Se tiver pacote no buffer, reenvia
+        for message in buffer:
+            #Coleta a mensagem do buffer e empacota
+            packet = IP_header + "|" + message
+            
+            router = ("127.0.0.1", 8100)
+
+            print(f"Mensagem re-enviada para o servidor: {message}")
+            client.sendto(packet.encode("utf-8"), router)
+        print('-------------------------------------------------')
+
+        resend = False
+
+#Função para lidar com ACKs
+def handleACK(message):
+    global duplicated_acks_count
+    global buffer
+    global resend
+    global rwnd
+    global queue_resend
+    global next_resend
+
+    #Obtém o tipo e conteúdo da mensagem e a janela de recepção
+    splitted_message = message.split("-")
+    message_type = splitted_message[0]
+    message_content = splitted_message[1]
+    rwnd = int(splitted_message[2][4:])
+
+    #Verifica se mensagem não é um ACK para remover do buffer
+    if message_type == "ack":
+        print(f"ACK da mensagem {message_content} recebido")
+        message_content = "message-" + message_content
+
+        #Se a mensagem estiver no buffer, é retirada e o timer é resetado
+        if message_content in buffer:
+            buffer.remove(message_content)
+            buffer_rtt[message_content] = {
+                "start": buffer_rtt[message_content]["start"],
+                "stop": time.perf_counter()
+            }
+            aimd.receiveNewAck()
+            resetTimer()
+        else:
+            if duplicated_acks_count[0] == message_content:
+                duplicated_acks_count = (message_content, duplicated_acks_count[1] + 1)
+            else:
+                duplicated_acks_count = (message_content, 1)
+            #Se receber 3 acks duplicados reenvia os pacotes
+            if duplicated_acks_count[1] == 3:
+                duplicated_acks_count = ("", 0)
+                aimd.receiveThreeDuplicatedAck()
+                if resend == False:
+                    resend_packet_thread = Thread(None, resendPacket)
+                    resend_packet_thread.start()
+                resend = True
 
 #Função que decodifica o pacote do roteador
 def receivePacket():
@@ -36,20 +192,20 @@ def receivePacket():
 
     #Converte a mensagem recebida
     message = packet.decode("utf-8").split("|")
+    message_content = message[2]
     
     # Obtém o endereço de origem
     ip_source = message[0].split(":")
     address = (ip_source[0], ip_source[1])
 
     # Retorna o conteúdo da mensagem e o endereço de origem
-    return message[2], address
+    return message_content, address
 
 #Função que envia solicitação de conexão para o servidor
 def connectWithServer(client_id):
     message = "connect-" + str(client_id)
 
     sendPacket(addr, message)
-    print(f"Mensagem enviada para o servidor: {message}")
 
 #Função executada antes do programa ser finalizado
 def exitHandler():
@@ -58,7 +214,6 @@ def exitHandler():
     message = "disconnect-" + str(client_id)
 
     sendPacket(addr, message)
-    print(f"Mensagem enviada para o servidor: {message}")
 
 #Registro da função executada antes do programa ser finalizado
 atexit.register(exitHandler)
@@ -88,7 +243,7 @@ if __name__ == "__main__":
     msg_received_string, address = receivePacket()
 
     # Verifica resposta de conexão enviada pelo servidor
-    if msg_received_string == "connected":
+    if msg_received_string.find('connected') != -1:
         print("Conexao estabelecida com o servidor")
         connectedWithServer = True
     else:
@@ -99,42 +254,46 @@ if __name__ == "__main__":
 
     while True:
         #Reseta variáveis
-        msg_to_answer = ""
         msg_received_string = ""
+        
+        print(f"Mensagens sem ACK: {buffer}")
 
-        #Gera número aleatório de até 10 casas
-        random_int_to_send = generateRandomNumber(
-            begin_number = 1,
-            number_of_decimals = random.randrange(1, 10)
-        )
+        #Se não tiver mensagens no buffer e não estiver re-enviando mensagens,
+        #Então envia vários pacotes de uma vez limitado pelo mínimo entre cwnd e rwnd
+        if len(buffer) == 0 and resend == False:
+            #Recalcula o intervalo de timeout a cada aproximadamente 64 pacotes
+            if len(buffer_rtt) > 64:
+                calculateTimeoutInterval()
+            print('----------------- Pacotes enviados ----------------')
+            print('cwnd', aimd.cwnd, 'rwnd', rwnd)
+                
+            while len(buffer) < min(aimd.cwnd, rwnd):
+                #Gera número aleatório de até 10 casas
+                random_int_to_send = generateRandomNumber(
+                    begin_number = 1,
+                    number_of_decimals = random.randrange(1, 10)
+                )
 
-        #Converte o número para string e envia com o tipo message
-        msg_to_send = "message-" + str(random_int_to_send)
-        print(f"Mensagem enviada para o servidor: {msg_to_send}")
-
-        #Envia a mensagem para o servidor
-        sendPacket(addr, msg_to_send)
+                #Converte o número para string e envia com o tipo message
+                msg_to_send = "message-" + str(random_int_to_send) + "?" + str(order)
+                sendPacket(addr, msg_to_send)
+                resetTimer()
+                order = order + 1
+            print('-------------------------------------------------')
 
         #Recebe a mensagem do servidor
         msg_received_string, address = receivePacket()
 
+        handleACK(msg_received_string)
+
         print(f"Mensagem recebida do servidor: {msg_received_string}")
 
-        #Envia mensagem para o servidor com o tipo message
-        msg_to_send = "message-" + msg_received_string + " ACK"
-        print(f"Mensagem enviada para o servidor: {msg_to_send}")
-
-        #Envia a mensagem para o servidor
-        sendPacket(addr, msg_to_send)
-
-        #Fecha a conexão e aguarda 10 segundos
-        
-        if(msg_received_string.__contains__("Janela de Recepção: 0")):
+        #Aguarda período de tempo de acordo com a janela de recepção
+        if rwnd == 0:
             for i in range(10):
                 print(str(10-i) + "s")
                 time.sleep(1)
-        else:
+        elif len(buffer) == 0:
             for i in range(3):
-                print(str(10-i) + "s")
+                print(str(3-i) + "s")
                 time.sleep(1)
-
